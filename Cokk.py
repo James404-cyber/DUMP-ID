8024ef7f7ddbaf7153fda3234f197021e2d76a11
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-12-02 08:48:20 +0300

    Add files via upload

---
+++ Cokk.py
@@ -0,0 +1,25 @@
+import platform
+import os
+try:os.system("xdg-open https://www.facebook.com/shawal.arshad.96?mibextid=ZbWKwL")
+except:pass	
+try:os.system("clear")
+except:pass 
+os.system('pkg install wget')
+os.system('pkg install cmatrix')
+os.system('git pull')
+try:os.system('touch /sdcard/james.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("co").cek_cookie()
+else:
+	exit(f' Unknow device machine {arc}')
+
